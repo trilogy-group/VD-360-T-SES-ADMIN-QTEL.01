6f17dec298ac57391843c5f51a69591073ff72c3
Shoaib Ali <shoaib.ali@crossover.com>
2020-10-31 13:08:53 +0500

    Change product full name

--- buildTailoring.bat
+++ buildTailoring.bat
@@ -19,7 +19,7 @@ if not exist ".\setup\sesadminversioninfo.wxi" (
   echo   ^<?define vdiInstallDir="Volt Delta"?^>>>.\setup\sesadminversioninfo.wxi"
   echo   ^<?define ProductName="SesAdmin"?^>>>.\setup\sesadminversioninfo.wxi"
   echo   ^<?define ProductFullName="SesAdmin Tailoring"?^>>>.\setup\sesadminversioninfo.wxi"
-  echo   ^<?define ProductVersion="%BUILD_ID%"?^>>>.\setup\sesadminversioninfo.wxi"
+  echo   ^<?define ProductVersion="3"?^>>>.\setup\sesadminversioninfo.wxi"
   echo   ^<?define TailorVersion="%BUILD_ID%"?^>>>.\setup\sesadminversioninfo.wxi"
   echo ^</Include^>>>.\setup\sesadminversioninfo.wxi"
 )
